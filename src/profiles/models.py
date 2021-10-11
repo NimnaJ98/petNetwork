1402eb8d04b2b1c85d88ba88c83b30253efcfdbb
Nimna <e1846608@bit.mrt.ac.lk>
2021-10-11 20:34:32 +0530

    creating relationship model

--- src/profiles/models.py
+++ src/profiles/models.py
@@ -19,7 +19,7 @@ class Profile(models.Model):
 
 
     def __str__(self):
-        return f"{self.user.username}-{self.created}"
+        return f"{self.user.username}-{self.created.strftime('%d-%m-%y')}"
 
 
     def save(self, *args, **kwargs):
@@ -35,4 +35,23 @@ class Profile(models.Model):
         self.slug = to_slug
         super().save(*args, **kwargs)
 
+STATUS_CHOICES = (
+    ('send','send'),
+    ('accepted','accepted')
+)
 
+class Relationship(models.Model):
+    
+    #everytime a particular profile is deleted, the relationship will also be deleted. 
+    sender = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='sender')
+    receiver = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='receiver')
+
+    #to determine the status of the choice. whether it's a sent invitation or an invitation accepted
+    status = models.CharField(max_length=8, choices=STATUS_CHOICES)
+
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return f"{self.sender}-{self.receiver}-{self.status}"
