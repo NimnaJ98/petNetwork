fc9cad07a052e5164823a8fbba1ab6e24910b16e
Nimna <e1846608@bit.mrt.ac.lk>
2021-10-20 09:10:22 +0530

    created the first template for posts app

--- src/SmaPetz/urls.py
+++ src/SmaPetz/urls.py
@@ -23,7 +23,9 @@ from .views import home_view
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',home_view,name='home-view'),
-    path('profiles/', include('profiles.urls', namespace='profiles'))
+    path('profiles/', include('profiles.urls', namespace='profiles')),
+    path('posts/', include('posts.urls', namespace='posts')),
+
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
